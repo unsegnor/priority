7facf1b2ff4cc0b8d06bbbde084c8e93b58951fc
vcalatayud <lacuentadevictor@gmail.com>
2022-07-03 14:12:05 +0200

    refactored

--- tests/tests.persistent.spec.js
+++ tests/tests.persistent.spec.js
@@ -13,41 +13,34 @@ describe('PersistentPrioritizedList', function(){
 
     prioritizedListTests()
 
-    it('recovering the list by name', async function(){
-        let greater = async function(item1, item2){
-            if (item1 > item2) return item1;
-            if (item2 > item1) return item2;
-        }
+    describe('given an existing list', function(){
+        let greater, repository
 
-        let repository = TestRepository()
+        this.beforeEach(async function(){
+            greater = async function(item1, item2){
+                if (item1 > item2) return item1;
+                if (item2 > item1) return item2;
+            }
+    
+            repository = TestRepository()
+    
+            let list = await Priority.getPersistentPrioritizedList(greater, listName, repository)
+    
+            await list.add('1')
+            await list.add('2')
+            await list.add('3')
+        })
 
-        let list = await Priority.getPersistentPrioritizedList(greater, listName, repository)
-
-        await list.add('1')
-        await list.add('2')
-        await list.add('3')
-
-        let recoveredList = await Priority.getPersistentPrioritizedList(greater, listName, repository)
-        let array = await recoveredList.toArray()
-        expect(array).to.deep.equal(['3','2','1'])
-    })
-
-    it('a list with a different name should be different', async function(){
-        greater = async function(item1, item2){
-            if (item1 > item2) return item1;
-            if (item2 > item1) return item2;
-        }
-
-        let repository = TestRepository()
-
-        let list = await Priority.getPersistentPrioritizedList(greater, listName, repository)
-
-        await list.add('1')
-        await list.add('2')
-        await list.add('3')
-
-        let recoveredList = await Priority.getPersistentPrioritizedList(greater, "differentName", repository)
-        let array = await recoveredList.toArray()
-        expect(array).to.deep.equal([])
+        it('recovering the list by name', async function(){
+            let recoveredList = await Priority.getPersistentPrioritizedList(greater, listName, repository)
+            let array = await recoveredList.toArray()
+            expect(array).to.deep.equal(['3','2','1'])
+        })
+    
+        it('a list with a different name should be different', async function(){
+            let recoveredList = await Priority.getPersistentPrioritizedList(greater, "differentName", repository)
+            let array = await recoveredList.toArray()
+            expect(array).to.deep.equal([])
+        })
     })
 })
