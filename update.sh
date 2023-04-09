8114da665c64fb1a1ab6b2e917f6e0fbd519f404
Víctor <lacuentadevictor@gmail.com>
2023-04-09 23:51:41 +0200

    load nvm

--- update.sh
+++ update.sh
@@ -2,9 +2,10 @@ git fetch
 git reset --hard
 git pull
 
-# Use nvm environment variables
-export NVM_DIR="$HOME/.nvm"
+# Load nvm
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
 nvm install
 nvm use
 nvm alias default "$(cat .nvmrc)"
